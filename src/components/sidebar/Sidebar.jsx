f776f619ca1fcc475de4855e264ac2706df413bd
Sorati <egor.dmitrenko.job@gmail.com>
2020-03-19 17:31:53 +0300

    add more items in sidebar

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -6,16 +6,40 @@ const Sidebar = ()=> {
 
     function test() {
         // let test = sideBar.current.style.display = 'none';
-        console.log(test);
+        console.log("test");
     }
     return(
 
         <aside  className={`${style.sidebar} ${style.sidebarShow}`} ref={sideBar} onClick={test}>
             <form action="POST">
-                <label>1<input type="text"/></label>
-                <label>1<input type="text"/></label>
-                <label>1<input type="text"/></label>
-                <label><input type="submit"/></label>
+                <label>ФИО птльзователя:<input type="text"/></label>
+                <label>Ответственное лицо:<input type="text"/></label>
+                <label>Кабинет<input type="text"/></label>
+
+                <label>Тип устройства:<input type="text"/></label>
+
+                <label>Фото:<input type="text"/></label>
+                {/*если не ПК*/}
+                <label>Производитель<input type="text"/></label>
+                <label>Модель:<input type="text"/></label>
+                <label>Серийный номер (s/n):<input type="text"/></label>
+                <label>Модельный номер (m/n):<input type="text"/></label>
+                <label>Уникальный номер (p/n):<input type="text"/></label>
+                {/*если ПК*/}
+                <label>Процессор:<input type="text"/></label>
+                <label>Оперативная память:<input type="text"/></label>
+                <label>Жесткий диск:<input type="text"/></label>
+                <label>Графический адаптер:<input type="text"/></label>
+                <label>IP адрес:<input type="text"/></label>
+                <label>Имя компьютера в сети:<input type="text"/></label>
+                <label>Доступ в интернет:<input type="text"/></label>
+                <label>Специальное ПО:<input type="text"/></label>
+
+                <label>Операционна система:<input type="text"/></label>
+                <label>Версия:<input type="text"/></label>
+
+                <label>Описание:<input type="text"/></label>
+                <label>Инвентарный номер:<input type="text"/></label>
             </form>
         </aside >
     )
