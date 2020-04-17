504f1a8ec887283bf3c5a580c73f1d002d02ff22
Sorati <egor.dmitrenko.job@gmail.com>
2020-04-17 12:55:45 +0300

    can add new person (no script)

--- src/components/modalAddItems/ModalAddItems.jsx
+++ src/components/modalAddItems/ModalAddItems.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import style from './ModalAddItems.module.css'
 import {NavLink} from "react-router-dom";
 
-class ModalAddItems extends React.PureComponent{
-    constructor(props){
+class ModalAddItems extends React.PureComponent {
+    constructor(props) {
         super(props);
         this.data = props.date.data;
         this.state = {
@@ -13,7 +13,7 @@ class ModalAddItems extends React.PureComponent{
 
     _getTypeList() {
         const list = [];
-        for(let type in this.data){
+        for (let type in this.data) {
             list.push(<option value={`${type}`} key={`${type}`}>{`${type}`}</option>)
         }
         return list
@@ -21,33 +21,44 @@ class ModalAddItems extends React.PureComponent{
 
     render() {
         return (
-        <div className={style.outsude}>
-            <div className={style.window}>
-                <NavLink className={style.btnClose} to={'/'}>X</NavLink>
-                <div className={style.content}>
-                        <form action="POST" onSubmit={(evn)=>{evn.preventDefault()}}>
+            <div className={style.outsude}>
+                <div className={style.window}>
+                    <NavLink className={style.btnClose} to={'/'}>X</NavLink>
+                    <div className={style.content}>
+                        <form action="POST" onSubmit={(evn) => {
+                            evn.preventDefault()
+                        }}>
+
                             <label htmlFor="styledSelect1" className={style.customSelect}>
-                                <select name="options" id="styledSelect1" >
+                                <select name="options" id="styledSelect1">
                                     <option value="none" hidden>-</option>
                                     {this._getTypeList()}
                                 </select>
                             </label>
-                            <label>Ф.И.О.<input type="text"/></label>
-                            <label>Отдел<input type="text"/></label>
-                            <label>Должность<input type="text"/></label>
-                            <label>Кабинет<input type="text"/></label>
 
-                            <label>Зав отдела<input type="checkbox"/></label>
-
-                            <input type="file" name="file" id="file" className={style.inputfile}/>
-                            <label htmlFor="file">Выберите фото</label>
-
-                            <button onSubmit={(evn)=>{evn.preventDefault()}}>Создать</button>
+                            <div className={style.content__person}>
+                                <label><input type="text" placeholder={'Ф.И.О.'}/></label>
+                                <label><input type="text" placeholder={'Отдел'}/></label>
+                                <label><input type="text" placeholder={'Должность'}/></label>
+                                <label><input type="text" placeholder={'Кабинет'}/></label>
+                                <label className={style.checkbox}><input type="checkbox"/>
+                                    <span>Зав отдела</span></label>
+                            </div>
+                            <div className={style.general}>
+                                <label><textarea name="description" rows="10"
+                                                 placeholder={'Описание'}></textarea></label>
+                                <input type="file" name="file" id="file" className={style.inputfile}/>
+                                <label htmlFor="file">Выберите фото</label>
+                                <button onSubmit={(evn) => {
+                                    evn.preventDefault()
+                                }}>Создать
+                                </button>
+                            </div>
                         </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
     }
 }
 
